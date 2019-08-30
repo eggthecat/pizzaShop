dcbca8160fe4785b0cb6d97a6588a116ff87eec6
Dominic Montelongo <montelongodominic@gmail.com>
2019-08-30 16:50:26 -0700

    Adjust var to achiece functionality in user interface

--- js/scripts.js
+++ js/scripts.js
@@ -22,23 +22,23 @@
 
 // business logic
 // Define Customer
-function Customer (firstName, lastName, phoneNumber, userId) {
-  this.firstName = firstName;
-  this.lastName = lastName;
-  this.phoneNumber = phoneNumber;
-  this.userId = userId;
-  }
-
-  Customer.prototype.name = function (firstName, lastName) {
-    return this.firstName + " " + this.lastName;
-    console.log(name);
-    console.log(this.name);
-    console.log(firstName);
-    console.log(this.firstName);
+// function Customer (firstName, lastName, phoneNumber, userId) {
+//   this.firstName = firstName;
+//   this.lastName = lastName;
+//   this.phoneNumber = phoneNumber;
+//   this.userId = userId;
+//   }
+//
+//   Customer.prototype.name = function (firstName, lastName) {
+//     return this.firstName + " " + this.lastName;
+//     console.log(name);
+//     console.log(this.name);
+//     console.log(firstName);
+//     console.log(this.firstName);
 
     // document.getElementById("demo1").innerHTML = this.name;
     // document.getElementById("demo2").innerHTML =
-  }
+  // }
 // Define order
 // function Order (orderNumber,orderAmount, pizzaSize, pizzaTopping)
 //   Customer.prototype.order = function () {
@@ -48,8 +48,8 @@ function Customer (firstName, lastName, phoneNumber, userId) {
 
 // user interface
 $(document).ready(function(){
-  $("#customerOrderSubmit").submit(function(event) {
-    event.preventDefualt()
+  $("form#orderInput").submit(function(event) {
+
 
     var firstName = ($("#firstName")).val();
     var lastName  = $("#lastName").val();
@@ -57,6 +57,7 @@ $(document).ready(function(){
     var pizzaToppings = $("").val();
     var pizzaPrice = $("").val();
     console.log(lastName);
+    console.log("#firstName");
     console.log(firstName);
 
   });
