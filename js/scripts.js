f382bc294988fc6daf77639c17b23b5070dda9a4
Dominic Montelongo <montelongodominic@gmail.com>
2019-08-31 14:02:06 -0700

    Adjust file layout: move mn obj & goals to plans.js

--- js/scripts.js
+++ js/scripts.js
@@ -1,25 +1,3 @@
-
-// Main Objectives
- //define what is doing what where how when why
-// Customer name that will print in name area
-// Customer pizzaSize selection that will print in size area
-// Customer pizzaToppings selection that will be listed in the toppings area
-// Customer price for entire order
-
-// Stretch Goals
-
-
-// Add sides for customer to selection
-// Add option for delivery and pickup
-// Add area for delivery information
-// Add coupon code and logic for price to be adjusted
-// Add sales tax to non-oregon orders
-// Add option for dietary restrictions
-// Add future delivery option
-// Add footer tabs for feedback, instagram(fake account), facebook (fake account), twitter (fak account), link to BBB, and language option for spanish
-// Add several more language options (Russian, Vietnamese, etc)
-
-
 // business logic
 // Define Customer
 // function Customer (firstName, lastName, phoneNumber, userId) {
@@ -48,16 +26,15 @@
 
 // user interface
 $(document).ready(function(){
-  $("form#orderInput").submit(function(event) {
+  $("#orderInput").submit(function(event) {
+    event.preventDefault()
 
-
-    var firstName = ($("#firstName")).val();
+    var firstName = $("#firstName").val();
     var lastName  = $("#lastName").val();
     var pizzaSize = $("").val();
     var pizzaToppings = $("").val();
     var pizzaPrice = $("").val();
     console.log(lastName);
-    console.log("#firstName");
     console.log(firstName);
 
   });
