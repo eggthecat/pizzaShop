472b4446b0984d45605e0954676fa95ccd29e3f5
Dominic Montelongo <montelongodominic@gmail.com>
2019-08-31 19:11:24 -0700

    Mofugy pizza consturctor, develop pizza prototype for price of pizza, and create method for output with document.getElementById

--- js/scripts.js
+++ js/scripts.js
@@ -7,33 +7,12 @@
 
 
 function pizza (size, toppings){
-
+  this.size = size,
+  this.topping = topping
 }
 
+pizza.prototype.price = (size, topping)
 
-// Define Customer
-// function Customer (firstName, lastName, phoneNumber, userId) {
-//   this.firstName = firstName;
-//   this.lastName = lastName;
-//   this.phoneNumber = phoneNumber;
-//   this.userId = userId;
-//   }
-//
-//   Customer.prototype.name = function (firstName, lastName) {
-//     return this.firstName + " " + this.lastName;
-//     console.log(name);
-//     console.log(this.name);
-//     console.log(firstName);
-//     console.log(this.firstName);
-
-    // document.getElementById("demo1").innerHTML = this.name;
-    // document.getElementById("demo2").innerHTML =
-  // }
-// Define order
-// function Order (orderNumber,orderAmount, pizzaSize, pizzaTopping)
-//   Customer.prototype.order = function () {
-//     return
-//   }
 
 
 
@@ -42,13 +21,11 @@ $(document).ready(function(){
   $("#orderInput").submit(function(event) {
     event.preventDefault()
 
-    var firstName = $("#firstName").val();
-    var lastName  = $("#lastName").val();
     var pizzaSize = $("").val();
     var pizzaToppings = $("").val();
     var pizzaPrice = $("").val();
-    console.log(lastName);
-    console.log(firstName);
+
+    document.getElementById("result").innerHTML = "Your " + pizzaSize + "pizza with " + pizzaToppings + "will be " + pizzaPrice";
 
   });
 });
